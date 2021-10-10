7058b9299d51171e66c0ab9b879a84b020a474b6
robertocajueiro <beto.cajueiro@gmail.com>
2021-10-10 05:23:47 -0300

    2.5. Criando um projeto Spring Boot com Spring Initializr
    realização do downgrade para a versão 2.1.7, indicada como ideal para a realização do curso

---
+++ algafood-api/src/test/java/com/algarworks/algafood/AlgafoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algarworks.algafood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgafoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
