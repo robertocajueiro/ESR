f4880d9820c6c193e29ca31964a68e860a065870
robertocajueiro <beto.cajueiro@gmail.com>
2021-10-10 07:06:10 -0300

    2.9. O que é injeção de dependências?

---
+++ exemplo-di/src/com/algaworks/di/notificacao/NotificadorSMS.java
@@ -0,0 +1,13 @@
+package com.algaworks.di.notificacao;
+
+import com.algaworks.di.modelo.Cliente;
+
+public class NotificadorSMS implements Notificador {
+	
+	@Override
+	public void notificar(Cliente cliente, String mensagem) {
+		System.out.printf("Notificando %s por SMS atraves do telefone %s: %s\n",
+				cliente.getNome(), cliente.getTelefone(), mensagem);
+	}
+
+}
