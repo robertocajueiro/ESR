f36f71c99021de544c74138528b2aefaa55acdd1
robertocajueiro <beto.cajueiro@gmail.com>
2021-10-16 12:08:49 -0300

    2.26. Acessando propriedades com @ConfigurationProperties

--- algafood-api/src/main/java/com/algarworks/algafood/di/notificacao/NotificadorEmail.java
+++ algafood-api/src/main/java/com/algarworks/algafood/di/notificacao/NotificadorEmail.java
@@ -1,6 +1,6 @@
 package com.algarworks.algafood.di.notificacao;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.algarworks.algafood.di.modelo.Cliente;
@@ -9,16 +9,13 @@ import com.algarworks.algafood.di.modelo.Cliente;
 @Component
 public class NotificadorEmail implements Notificador {
 	
-	@Value("${notificador.email.host-servidor}")
-	private String host;
-	
-	@Value("${notificador.email.porta-servidor}")
-	private Integer porta;
+	@Autowired
+	private NotificadorProperties properties;
 	
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
-		System.out.println("Host: " + host);
-		System.out.println("Porta: " + porta);
+		System.out.println("Host: " + properties.getHostServidor());
+		System.out.println("Porta: " + properties.getPortaServidor());
 		
 		System.out.printf("Notificando %s atraves do e-mail %s: %s\n",
 				cliente.getNome(), cliente.getEmail(), mensagem);
