1ab14844c8cfcff92e7e367b2b91c9cb1e8c45c0
robertocajueiro <beto.cajueiro@gmail.com>
2021-10-30 16:23:53 -0300

    4.21. Corrigindo o Status HTTP para resource inexistente

--- algafood-api/src/main/java/com/algarworks/algafood/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/algarworks/algafood/api/controller/CozinhaController.java
@@ -37,17 +37,13 @@ public class CozinhaController {
 	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
 		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
 		
-//		return ResponseEntity.status(HttpStatus.OK).body(cozinha);
-//		return ResponseEntity.ok(cozinha);
+		if (cozinha != null) {
+			return ResponseEntity.ok(cozinha);
+		}
 		
-		
-		HttpHeaders headers = new HttpHeaders();
-		headers.add(HttpHeaders.LOCATION, "http://localhost.8080/cozinhas");
-		
-		return ResponseEntity
-				.status(HttpStatus.FOUND)
-				.headers(headers)
-				.build();
+//		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		return ResponseEntity.notFound().build();
+			
 	}
 	
 }
