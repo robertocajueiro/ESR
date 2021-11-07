7e5bcfcc21d5f8aa33beef769f232d79657a74db
robertocajueiro <beto.cajueiro@gmail.com>
2021-11-07 08:40:14 -0300

    4.33. Analisando solução para atualização parcial de recursos com PATCH

--- algafood-api/src/main/java/com/algarworks/algafood/api/controller/RestauranteController.java
+++ algafood-api/src/main/java/com/algarworks/algafood/api/controller/RestauranteController.java
@@ -1,12 +1,14 @@
 package com.algarworks.algafood.api.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -59,24 +61,46 @@ public class RestauranteController {
 		}		
 	}
 	
-		@PutMapping("/{restauranteId}")
-		public ResponseEntity<?> atualizar(@PathVariable Long restauranteId,
+	@PutMapping("/{restauranteId}")
+	public ResponseEntity<?> atualizar(@PathVariable Long restauranteId,
 				@RequestBody Restaurante restaurante){
 			
-		try {	
-			Restaurante restauranteAtual = restauranteRepository.buscar(restauranteId);
-			
-			if (restauranteAtual != null) {
-				BeanUtils.copyProperties(restaurante, restauranteAtual, "id");
+			try {	
+				Restaurante restauranteAtual = restauranteRepository.buscar(restauranteId);
 				
-				restauranteAtual = cadastroRestaurante.salvar(restauranteAtual);
-				return ResponseEntity.ok(restauranteAtual);
+				if (restauranteAtual != null) {
+					BeanUtils.copyProperties(restaurante, restauranteAtual, "id");
+					
+					restauranteAtual = cadastroRestaurante.salvar(restauranteAtual);
+					return ResponseEntity.ok(restauranteAtual);
+				}
+				
+				return ResponseEntity.notFound().build();
+			} catch (EntidadeNaoEncontradaException e) {
+				return ResponseEntity.badRequest()
+						.body(e.getMessage());
 			}
-			
-			return ResponseEntity.notFound().build();
-		} catch (EntidadeNaoEncontradaException e) {
-			return ResponseEntity.badRequest()
-					.body(e.getMessage());
 		}
+	
+	@PatchMapping("/{restauranteId}")
+	public ResponseEntity<?> atualizarParcial(@PathVariable Long restauranteId,
+			@RequestBody Map<String, Object> campos) {
+		Restaurante restauranteAtual = restauranteRepository.buscar(restauranteId);
+		
+		if(restauranteAtual == null) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		merge(campos, restauranteAtual);
+		
+		return atualizar(restauranteId, restauranteAtual);
 	}
+
+	private void merge(Map<String, Object> camposOrigem, Restaurante restauranteDestino) {
+		camposOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+			System.out.println(nomePropriedade + " = " + valorPropriedade);
+		});
+	}
+	
+		
 }
