4c57e42caff5f75e83042c4dfff7a3e7dd1265a9
robertocajueiro <beto.cajueiro@gmail.com>
2021-11-13 10:07:48 -0300

    5.9. Usando queries JPQL customizadas com @Query

--- algafood-api/src/main/java/com/algarworks/algafood/domain/repository/RestauranteRepository.java
+++ algafood-api/src/main/java/com/algarworks/algafood/domain/repository/RestauranteRepository.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.algarworks.algafood.domain.model.Restaurante;
@@ -15,8 +17,12 @@ public interface RestauranteRepository extends JpaRepository<Restaurante, Long>{
 	// Usando keyword Between
 	List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 	
+	@Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+	List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinha);
+	
+	
 	//And
-	List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha);
+	//List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha);
 
 	// Limit - só o primeiro registro
 	Optional<Restaurante> findFirstRestauranteByNomeContaining(String nome);
