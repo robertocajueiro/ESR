54799f7fc7d4681dc5df1f7d450e4722d5415216
robertocajueiro <beto.cajueiro@gmail.com>
2021-11-20 17:34:24 -0300

    6.2. Mapeando relacionamento muitos-para-muitos com @ManyToMany

--- algafood-api/src/main/java/com/algarworks/algafood/domain/model/Restaurante.java
+++ algafood-api/src/main/java/com/algarworks/algafood/domain/model/Restaurante.java
@@ -1,6 +1,8 @@
 package com.algarworks.algafood.domain.model;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -8,9 +10,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -31,9 +33,15 @@ public class Restaurante {
 	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
 	
-//	@JsonIgnore
 	@ManyToOne
 	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
+	
+	@ManyToMany
+	@JoinTable(name = "restaurante_forma_pagamento",
+			joinColumns = @JoinColumn (name = "restaurante_id"),
+			inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
+		
+	private List<FormaPagamento> formasPagamento = new ArrayList<>();
 
 }
