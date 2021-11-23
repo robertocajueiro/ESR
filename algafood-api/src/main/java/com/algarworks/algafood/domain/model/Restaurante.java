599e0d087c2d2463b1b00cdfdb416bafebd01d13
Roberto Cajueiro <beto.cajueiro@gmail.com>
2021-11-23 15:15:20 -0300

    6.14. Resolvendo o Problema do N+1 com fetch join na JPQL

--- algafood-api/src/main/java/com/algarworks/algafood/domain/model/Restaurante.java
+++ algafood-api/src/main/java/com/algarworks/algafood/domain/model/Restaurante.java
@@ -8,7 +8,6 @@ import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -22,7 +21,6 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -43,9 +41,8 @@ public class Restaurante {
 	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
 	
-	@JsonIgnore
-//	@JsonIgnoreProperties("hibernateLazyInitializer")
-	@ManyToOne(fetch = FetchType.LAZY)
+//	@JsonIgnore
+	@ManyToOne //(fetch = FetchType.LAZY)
 	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
 	
@@ -64,20 +61,15 @@ public class Restaurante {
 	private LocalDateTime dataAtualizacao;
 	
 	
-	@JsonIgnore
-	@ManyToMany(fetch = FetchType.EAGER)
-	@JoinTable(name = "restaurante_forma_pagamento", 
-			joinColumns = @JoinColumn (name = "restaurante_id"), 
-			inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
+//	@JsonIgnore
+	@ManyToMany
+	@JoinTable(name = "restaurante_forma_pagamento",
+		joinColumns = @JoinColumn(name = "restaurante_id"),
+		inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
 	private List<FormaPagamento> formasPagamento = new ArrayList<>();
 	
 	@JsonIgnore
 	@OneToMany(mappedBy = "restaurante")
 	private List<Produto> produtos = new ArrayList<>();
 	
-	
-	
-	
-	
-
 }
--- algafood-api/src/main/java/com/algarworks/algafood/domain/repository/RestauranteRepository.java
+++ algafood-api/src/main/java/com/algarworks/algafood/domain/repository/RestauranteRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -14,6 +15,9 @@ import com.algarworks.algafood.domain.model.Restaurante;
 public interface RestauranteRepository 
 		extends CustomJpaRepository<Restaurante, Long>, RestauranteRepositoryQueries,
 		JpaSpecificationExecutor<Restaurante>{
+	
+	@Query("from Restaurante r join fetch r.cozinha join fetch r.formasPagamento")
+	List<Restaurante> findAll();
 	
 	// Usando keyword Between
 	List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
