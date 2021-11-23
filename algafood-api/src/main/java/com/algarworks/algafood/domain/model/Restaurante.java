3e01348ce56ce4a8fa4985a0ce4676753ef94279
Roberto Cajueiro <beto.cajueiro@gmail.com>
2021-11-23 13:30:43 -0300

    6.13. Alterando a estratégia de fetching para Eager Loading

--- algafood-api/src/main/java/com/algarworks/algafood/domain/model/Restaurante.java
+++ algafood-api/src/main/java/com/algarworks/algafood/domain/model/Restaurante.java
@@ -43,8 +43,8 @@ public class Restaurante {
 	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
 	
-	//@JsonIgnore
-	@JsonIgnoreProperties("hibernateLazyInitializer")
+	@JsonIgnore
+//	@JsonIgnoreProperties("hibernateLazyInitializer")
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
@@ -65,8 +65,9 @@ public class Restaurante {
 	
 	
 	@JsonIgnore
-	@ManyToMany
-	@JoinTable(name = "restaurante_forma_pagamento", joinColumns = @JoinColumn (name = "restaurante_id"),
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "restaurante_forma_pagamento", 
+			joinColumns = @JoinColumn (name = "restaurante_id"), 
 			inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
 	private List<FormaPagamento> formasPagamento = new ArrayList<>();
 	
