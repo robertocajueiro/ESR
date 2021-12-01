2b39136cb5a0091d261644a5cf7ffe94938218c4
Roberto Cajueiro <beto.cajueiro@gmail.com>
2021-12-01 13:59:04 -0300

    7.10. Adicionando dados de testes com callback do Flyway

--- algafood-api/src/main/java/com/algarworks/algafood/domain/model/Restaurante.java
+++ algafood-api/src/main/java/com/algarworks/algafood/domain/model/Restaurante.java
@@ -61,7 +61,7 @@ public class Restaurante {
 	private LocalDateTime dataAtualizacao;
 	
 	
-//	@JsonIgnore
+	@JsonIgnore
 	@ManyToMany
 	@JoinTable(name = "restaurante_forma_pagamento",
 		joinColumns = @JoinColumn(name = "restaurante_id"),
