208ceae3280a247f91055fd14eebbe62eea69c7e
robertocajueiro <beto.cajueiro@gmail.com>
2022-01-14 20:23:25 -0300

    8.9. Desafio: usando a exception NegocioException

--- algafood-api/src/main/java/com/algarworks/algafood/api/controller/RestauranteController.java
+++ algafood-api/src/main/java/com/algarworks/algafood/api/controller/RestauranteController.java
@@ -18,6 +18,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algarworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algarworks.algafood.domain.exception.NegocioException;
 import com.algarworks.algafood.domain.model.Restaurante;
 import com.algarworks.algafood.domain.repository.RestauranteRepository;
 import com.algarworks.algafood.domain.service.CadastroRestauranteService;
@@ -46,19 +48,27 @@ public class RestauranteController {
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public Restaurante adicionar(@RequestBody Restaurante restaurante){
-		return cadastroRestaurante.salvar(restaurante);
+		try {
+			return cadastroRestaurante.salvar(restaurante);
+		} catch (EntidadeNaoEncontradaException e) {
+			throw new NegocioException(e.getMessage());
+		}
 	}
 	
 	@PutMapping("/{restauranteId}")
 	public Restaurante atualizar(@PathVariable Long restauranteId,
-				@RequestBody Restaurante restaurante){
+		@RequestBody Restaurante restaurante){
 	
-				Restaurante restauranteAtual = cadastroRestaurante.buscarOuFalhar(restauranteId);
+			Restaurante restauranteAtual = cadastroRestaurante.buscarOuFalhar(restauranteId);
 
-					BeanUtils.copyProperties(restaurante, restauranteAtual, 
-							"id", "formasPagamento", "endereco", "dataCadastro", "produtos");
-					
-					return cadastroRestaurante.salvar(restauranteAtual);
+			BeanUtils.copyProperties(restaurante, restauranteAtual, 
+				"id", "formasPagamento", "endereco", "dataCadastro", "produtos");
+			
+			try {
+				return cadastroRestaurante.salvar(restauranteAtual);
+			} catch (EntidadeNaoEncontradaException e) {
+				throw new NegocioException(e.getMessage());
+			}
 		}
 	
 	@PatchMapping("/{restauranteId}")
