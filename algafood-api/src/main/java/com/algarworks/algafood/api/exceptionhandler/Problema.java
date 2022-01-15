b1a40b40afb142bc612cdfe2bf389aa9d7fa6420
robertocajueiro <beto.cajueiro@gmail.com>
2022-01-14 23:36:36 -0300

    8.12. Tratando exceções em nível de controlador com @ExceptionHandler

---
+++ algafood-api/src/main/java/com/algarworks/algafood/api/exceptionhandler/Problema.java
@@ -0,0 +1,16 @@
+package com.algarworks.algafood.api.exceptionhandler;
+
+import java.time.LocalDateTime;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Builder
+public class Problema {
+	
+	private LocalDateTime dataHora;
+	private String mensagem;
+
+}
